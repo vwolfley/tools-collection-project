f666fa892ccd17794074063e764383be1ad0a6aa
vern wolfley <vern.wolfley@gmail.com>
2025-02-22 19:40:33 -0700

    adding google oauth

---
+++ src/auth/passport.js
@@ -0,0 +1,32 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User"); // Example user model
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (!user) {
+          user = await User.create({ googleId: profile.id, name: profile.displayName });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+module.exports = passport;
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,22 @@
 const express = require("express");
 const router = express.Router();
 
+// Home page info
+const homeInfo = `
+<h2>Welcome to the Tools Project API!</h2>
+<p>Sign in to the Tools Project API:</p>
+<menu>
+  <li><button id="logout">logout</button></li>
+  <li><button id="google">Log in with Google</button></li>
+  <li><button id="github">Log in with GitHub</button></li>
+</menu>
+`;
+
+// Home route
 router.get("/", (req, res) => {
   // #swagger.ignore = true
-  res.send("<h2>Welcome to the Tools Project API!</h2><p>Sign in with Google</p>");
+  res.send(homeInfo);
 });
-
 
 router.use("/users", require("./users"));
 router.use("/userTools", require("./userTools"));
