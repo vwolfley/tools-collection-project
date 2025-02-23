3b0b67e123cbf0a2d2641e576a7f93b4316770f0
vern wolfley <vern.wolfley@gmail.com>
2025-02-22 17:27:19 -0700

    add toolSet model

---
+++ src/models/toolSet-model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const toolSetSchema = new Schema({
+  name: String,
+  brand: String,
+  category: {
+    type: String,
+    enum: ["Hand Tools", "Power Tools", "Mechanic Tools", "Woodworking", "Plumbing", "Other"]
+  },
+  image_url: String,
+  description: String,
+  tools: [{ type: Schema.Types.ObjectId, ref: "Tool" }],
+});
+
+const ToolSet = mongoose.model("ToolSet", toolSetSchema);
+
+module.exports = ToolSet;
+
